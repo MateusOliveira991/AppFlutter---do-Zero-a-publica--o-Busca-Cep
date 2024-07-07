33e155389081533f5d2bfb0e6b29b0f2e77338a1
MateusOliveira991 <mateusaugusto9.91@gmail.com>
2024-07-06 21:31:13 -0300

    Feat: Componente para tratar os erros

---
+++ lib/services/cep_service.dart
@@ -0,0 +1,33 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class CepService {
+  // Função para validar o CEP
+  bool validarCep(String cep) {
+    final RegExp cepExp = RegExp(r'^\d{8}$');
+    return cepExp.hasMatch(cep);
+  }
+
+  // Função assíncrona para buscar o CEP
+  Future<Map<String, dynamic>> buscarCep(String cep) async {
+    String url = 'https://viacep.com.br/ws/$cep/json/';
+
+    // Faz a requisição HTTP e espera pela resposta
+    http.Response response = await http.get(Uri.parse(url));
+
+    // Verifica o código de status da resposta
+    if (response.statusCode == 200) {
+      // Decodifica o JSON retornado
+      Map<String, dynamic> dados = json.decode(response.body);
+
+      // Retorna os dados ou erro
+      if (dados.containsKey('erro')) {
+        throw Exception('CEP não encontrado');
+      } else {
+        return dados;
+      }
+    } else {
+      throw Exception('Erro ao buscar CEP: ${response.statusCode}');
+    }
+  }
+}
