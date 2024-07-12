71f0309a527c91f464094ead6eab840376e658da
MateusOliveira991 <mateusaugusto9.91@gmail.com>
2024-07-11 22:49:15 -0300

    Feat: Alteração de layout com animação

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-
 import 'package:your_app_buscaenderecoporcep/widgets/quadrocep.dart';
-
-
+import 'package:your_app_buscaenderecoporcep/widgets/iconemap.dart'; 
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -10,22 +8,21 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      
-      body:SingleChildScrollView(
-            child: Column(
-              children: <Widget>[
-                SizedBox(
-                  height: 10,
-                ),
-                Padding(
-                  padding: EdgeInsets.only(left: 10, right: 10, top: 35),
-                  child: Quadrocep(),
-                ),
-              ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            SizedBox(height: 10),
+            Padding(
+              padding: EdgeInsets.only(left: 10, right: 10, top: 35),
+              child: Quadrocep(), 
             ),
-          ),
-      );
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 60),
+              child: Iconemap(), 
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
-
-
